e9b418e5bdad59553cfd7f4aae67c77e177b74de
truethatplaya <alina.hernandez.ca@gmail.com>
2021-04-06 22:23:35 -0700

    added math.random functions for each character. tested and working in the console.log. prompt is cpopping up when page is refreshed.

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,59 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function generatePassword() {
+  lengthPassword();
+  lower();
+  upper();
+  number();
+  special();
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var number = getRandomNumber;
+var upper = getRandomUpper;
+var lower = getRandomLower;
+var special = getRandomSpecial;
+var passwordLength = 0;
+// var lowercasePrompt = "";
+
+// Write password to the #password input
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+console.log(getRandomLower());
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+console.log(getRandomUpper());
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+console.log(getRandomNumber());
+
+function getRandomSpecial() {
+  var special = "!()*+,-./:~";
+  return special[Math.floor(Math.random() * special.length)];
+}
+
+console.log(getRandomSpecial());
+
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+// }
+
+function lengthPassword() {
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  var passwordLength = length;
+  console.log(typeof length);
+}
+
+// var caseLower = prompt("Would you like lowercase characters in your password?");
+// var caseUpper = prompt("Would you like uppercase characters in your password?");
